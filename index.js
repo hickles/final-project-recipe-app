172620a305e80d27dd4fff6fa0f3aa2d694888ff
hickles <156871133+hickles@users.noreply.github.com>
2024-04-15 23:28:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mealImage = document.getElementById('meal-image');
 const mealName = document.getElementById('meal-name');
 const ingredientsList = document.getElementById('ingredients-list');
 const instructions = document.getElementById('instructions');
-const recipeDetails = document.getElementById('recipe-details');
 
 btn.addEventListener('click', () => {
     fetch('https://www.themealdb.com/api/json/v1/1/random.php')
