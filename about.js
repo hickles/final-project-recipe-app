47fae4019af5b4ff08126d4314b941643a01cf63
hickles <156871133+hickles@users.noreply.github.com>
2024-04-20 17:31:10 -0400

    Update about.js

--- about.js
+++ about.js
@@ -4,8 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const recipeContainer = document.getElementById('recipeContainer2');
 
     searchForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Prevent form from submitting
-
+    
         fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInput.value.trim()}`)
             .then(response => response.json())
             .then(data => displayRecipes(data.meals));
